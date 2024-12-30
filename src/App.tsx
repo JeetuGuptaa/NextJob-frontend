1822a25a1f5cbdf069b6c6b53c0010864ecbc8dc
Jeetu Gupta <jeetu270602@gmail.com>
2024-12-30 16:22:41 +0530

    frontend, tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1 className="flex items-center">
+        <img src="./nextJob-NBG.png" height="72" width="72" />
+
+        <span className="font-medium text-4xl bg-gradient-to-br from-[#002642] via-[#2fb1df] to-[#002642] bg-clip-text text-transparent">
+          NextJob
+        </span>
+      </h1>
+    </>
+  );
+}
+
+export default App;
+
